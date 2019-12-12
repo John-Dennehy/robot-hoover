37bebf849e10e131d765f271796c454c4fb53db0
JDtheGeek <john.dennehy@gmail.com>
2019-12-12 17:23:11 +0000

    Adds test to check #dirtCollected starts at 0

--- tests/robot_hoover.test.js
+++ tests/robot_hoover.test.js
@@ -3,15 +3,23 @@ import RobotHoover from '../lib/robot_hoover'
 describe('RobotHoover', () => {
   const grid = { x: 5, y: 5 }
   const dirtLocations = [{ x: 1, y: 0 }, { x: 2, y: 2 }, { x: 2, y: 3 }]
-  const initialPosition = { x: 1, y: 2 }
+  const startPosition = { x: 1, y: 2 }
   const instructions = 'NNESEESWNWW'
-  const hoover = new RobotHoover(initialPosition)
+  const hoover = new RobotHoover(startPosition)
 
   it('returns current position', () => {
     expect(hoover.location()).toEqual({ x: 1, y: 2 })
+    expect(hoover.location()).not.toEqual({ x: 0, y: 0 })
   })
 
-  it('does not return a different position', () => {
-    expect(hoover.location()).not.toEqual({ x: 10, y: 6 })
+  it('to start with zero #dirtCollected', () => {
+    expect(hoover.dirtCollected()).toEqual(0)
+  })
+
+  it('to return its location matching the inital position', () => {
+    const initialPosition = { x: 0, y: 0 }
+    const newHoover = new RobotHoover(initialPosition)
+    expect(newHoover.location()).toEqual({ x: 0, y: 0 })
+    expect(newHoover.location()).not.toEqual({ x: 1, y: 2 })
   })
 })

1ace51c4dfd3b96f7d6f3e41d7091ac498e47427
JDtheGeek <john.dennehy@gmail.com>
2019-12-12 16:11:10 +0000

    updates notes to include additional requirement to output data in a file.  Will assume output.txt to match input.txt

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-export function output (x, y, dirtCount) {
+export function output(x, y, dirtCount) {
   const outputString = x + ' ' + y + '\n' + dirtCount
   console.log(outputString)
   return outputString
 }
 
-export function isDirty (coords, dirtPositions) {
+export function isDirty(coords, dirtPositions) {
   let output = false
   dirtPositions.forEach(function (value) {
     if (value.x === coords.x && value.y === coords.y) {

4741bb3b437cf84b38d06350c0dbb04687423607
JDtheGeek <john.dennehy@gmail.com>
2019-12-12 16:57:57 +0000

    fixes syntax error in test and passes test

--- tests/robot_hoover.test.js
+++ tests/robot_hoover.test.js
@@ -1,15 +1,13 @@
 import RobotHoover from '../lib/robot_hoover'
 
 describe('RobotHoover', () => {
-  beforeEach(() => {
-    const grid = { x: 5, y: 5 }
-    const dirtLocations = [{ x: 1, y: 0 }, { x: 2, y: 2 }, { x: 2, y: 3 }]
-    const initialPosition = { x: 1, y: 2 }
-    const instructions = 'NNESEESWNWW'
-    const hoover = new RobotHoover()
-  })
+  const grid = { x: 5, y: 5 }
+  const dirtLocations = [{ x: 1, y: 0 }, { x: 2, y: 2 }, { x: 2, y: 3 }]
+  const initialPosition = { x: 1, y: 2 }
+  const instructions = 'NNESEESWNWW'
+  const hoover = new RobotHoover()
 
   it('returns current position', () => {
-    expect(hoover.location).toEqual({ x: 1, y: 2 })
+    expect(hoover.location()).toEqual({ x: 1, y: 2 })
   })
 })

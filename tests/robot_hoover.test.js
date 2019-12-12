5e077d05e45d383630760a53d7eca3c17e45dda9
JDtheGeek <john.dennehy@gmail.com>
2019-12-12 23:07:08 +0000

    refactoring

--- tests/robot_hoover.test.js
+++ tests/robot_hoover.test.js
@@ -12,19 +12,20 @@ describe('RobotHoover', () => {
     expect(hoover.position).not.toEqual({ x: 0, y: 0 })
   })
 
-  it('to start with zero #dirtCollected', () => {
+  it('starts with zero #dirtCollected', () => {
     expect(hoover.dirtCollected).toEqual(0)
     expect(hoover.dirtCollected).not.toBeGreaterThan(0)
   })
 
-  it('to increase #dirtCollected by 1 if position is dirty', () => {
+  it('increases #dirtCollected by 1 if position is dirty', () => {
     const hoover = new RobotHoover({ x: 2, y: 2 })
+    expect(hoover.dirtCollected).toEqual(0)
     hoover.setDirtyAreas(dirtLocations)
     hoover.checkForDirt()
     expect(hoover.dirtCollected).toEqual(1)
   })
 
-  it('to return its location matching the inital position', () => {
+  it('returns its location matching the inital position', () => {
     const initialPosition = { x: 0, y: 0 }
     const hoover = new RobotHoover(initialPosition)
     expect(hoover.position).toEqual({ x: 0, y: 0 })

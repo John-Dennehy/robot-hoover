b58fd548f6967fc492f7268b2c8a34c932ff415c
JDtheGeek <john.dennehy@gmail.com>
2019-12-19 15:18:18 +0000

    Restructure to remove es6 class

--- index.js
+++ index.js
@@ -2,32 +2,112 @@
 const fs = require('fs')
 const util = require('util')
 const readFile = util.promisify(fs.readFile)
-const RobotHoover = require('./lib/robot_hoover')
+// const RobotHoover = require('./lib/robot_hoover')
+
+let roomDimensions = { x: 0, y: 0 }
+let hooverPosition = { x: 0, y: 0 }
+const dirtCollected = []
+let dirtyPatches = []
+let route = []
+
+startHoover()
 
 async function startHoover(filePath = './input.txt') {
   try {
-    const hoover = new RobotHoover()
     const fileContent = await readFile(filePath, 'utf8')
     console.log(`Reading instructions from ${filePath}`, '\n' + fileContent)
-    hoover.setInstructions(fileContent)
-    console.log(hoover)
-    return hoover
+    setInstructions(fileContent)
+    runRoute()
   } catch (err) {
     console.error(err)
   }
 }
 
-function createInstructions(fileContent) {
-
+function setInstructions(data) {
+  const dataArray = data.toString().split('\n')
+  console.log('Data from input: ', dataArray)
+  setRoomDimensions(dataArray.shift())
+  setInitialPosition(dataArray.shift())
+  setRoute(dataArray.pop())
+  setDirtyPatches(dataArray)
 }
 
-function start() {
-  console.log('Started...')
-  const hoover = startHoover()
-  console.log(hoover)
-  // console.log('Following route')
-  // hoover.runRoute()
-  // console.log('Finished')
+function setRoomDimensions(roomData) {
+  const array = roomData.split(' ')
+  const dimensions = { x: Number(array[0]), y: Number(array[1]) }
+  roomDimensions = dimensions
+  return dimensions
 }
 
-start()
+function setInitialPosition(positionData) {
+  const array = positionData.split(' ')
+  hooverPosition = { x: Number(array[0]), y: Number(array[1]) }
+  return hooverPosition
+}
+
+function setRoute(routeData) {
+  route = routeData.split('')
+  return route
+}
+
+function runRoute() {
+  if (route.length > 0) {
+    route.forEach(function (direction) {
+      move(direction)
+    })
+  }
+}
+
+function move(direction) {
+  switch (direction) {
+    case 'N':
+      hooverPosition.y += 1
+      break
+    case 'S':
+      hooverPosition.y -= 1
+      break
+    case 'E':
+      hooverPosition.x += 1
+      break
+    case 'W':
+      hooverPosition.x -= 1
+      break
+    default:
+      break
+  }
+  hooverPosition = checkForWall(hooverPosition)
+  console.log(hooverPosition)
+  return hooverPosition
+}
+
+function vacuum() {
+  if (isDirty()) {
+    const index = dirtyPatches.indexOf(hooverPosition)
+    const vacuumedPatch = dirtyPatches.splice(index, 1)
+    dirtCollected.push(vacuumedPatch)
+  }
+}
+
+function checkForWall(newPosition) {
+  let xPos = newPosition.x
+  let yPos = newPosition.y
+  const xDim = roomDimensions.x
+  const yDim = roomDimensions.y
+  if (xPos < 0) { xPos = 0 }
+  if (yPos < 0) { yPos = 0 }
+  if (xPos > xDim) { xPos = xDim }
+  if (yPos > yDim) { yPos = yDim }
+
+  return { x: xPos, y: yPos }
+}
+
+function isDirty() {
+  const xPos = hooverPosition.x
+  const yPos = hooverPosition.y
+  return dirtyPatches.some(area => area.x === xPos && area.y === yPos)
+}
+
+function setDirtyPatches(array) {
+  dirtyPatches = array
+  return array
+}

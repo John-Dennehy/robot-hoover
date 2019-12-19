84a257ba8f555f413e0ef43d478229c645ab3f5a
JDtheGeek <john.dennehy@gmail.com>
2019-12-19 13:39:31 +0000

    Fixes initial async issue that had me stumped!!

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 #!/usr/bin/env node
+const fs = require('fs')
+const util = require('util')
+const readFile = util.promisify(fs.readFile)
 const RobotHoover = require('./lib/robot_hoover')
-const Input = require('./lib/input')
 
-function run () {
-  const hoover = new RobotHoover()
-  hoover.getInstructions()
-  hoover.runRoute()
+async function getInput(filePath = './input.txt') {
+  try {
+    const fileContent = await readFile(filePath, 'utf8')
+    console.log(fileContent)
+    createInstructions(fileContent)
+  } catch (err) {
+    console.error(err)
+  }
 }
+
+function createInstructions(fileContent) {
+  console.log('creating instructions from: ', fileContent)
+}
+
+function start() {
+  console.log('Started...')
+  getInput()
+  // const hoover = new RobotHoover()
+  // console.log('Reading input.txt file')
+  // console.log('Following route')
+  // hoover.runRoute()
+  // console.log('Finished')
+}
+
+start()
